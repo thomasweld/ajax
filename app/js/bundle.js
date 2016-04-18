560f8643db1350798a83a7238b862e31f350284f
thomasweld <thomas.weld@gmail.com>
2016-04-18 19:05:08 -0400

    most form types working

--- app/js/bundle.js
+++ app/js/bundle.js
@@ -9861,9 +9861,24 @@ var baseURL = 'http://json-data.herokuapp.com/forms';
 // get form content area in dom
 var form_content = (0, _jquery2['default'])('.form_content');
 
+// form textarea
+function formTemplateTextarea(type, label, id, icon, options) {
+  return '\n  <div id="' + id + '">\n    <span class="fa ' + icon + '"></span>\n    <input placeholder="' + label + '" type="' + type + '" />\n  </div>\n  ';
+}
+
 // form template
-function formTemplate(type, label, id, icon, options) {
-  return '\n  <p>' + type + ' ' + label + '\n  ';
+function formTemplate(type, label, id, icon) {
+  return '\n  <div id="' + id + '">\n    <span class="fa ' + icon + '"></span>\n    <input placeholder="' + label + '" type="' + type + '" />\n  </div>\n  ';
+}
+
+// form template for options
+function formTemplateOptions(type, label, id, icon, options) {
+  return '\n  <div class="' + id + '">\n    <input list="' + id + '" type="' + type + '" name="' + label + '" placeholder="' + label + '" /></label>\n    <datalist id=' + id + '>\n  ';
+}
+
+// form template for options
+function formOptionList(option) {
+  return '\n  <option value="' + option + '">\n  ';
 }
 
 // get json data into variable as jquery object
@@ -9874,8 +9889,52 @@ console.log(request);
 request.then(function (response) {
   //console.log(response);
   response.forEach(function (inputArea) {
-    var html = formTemplate(inputArea.type, inputArea.label);
-    form_content.append(html);
+
+    if (inputArea.type === 'textarea') {
+      var htmlTextarea = formTemplateTextarea(inputArea.type, inputArea.label, inputArea.id, inputArea.icon, inputArea.options);
+      form_content.append(htmlTextarea);
+    } else if (inputArea.options.length === 0) {
+      var html = formTemplate(inputArea.type, inputArea.label, inputArea.id, inputArea.icon);
+      form_content.append(html);
+    } else {
+      // stores results of formTemplateOptions function above in htmlOptions variable
+      var htmlOptions = formTemplateOptions(inputArea.type, inputArea.label, inputArea.id, inputArea.icon, inputArea.options);
+
+      // add initial input tag via htmlOptions
+      form_content.append(htmlOptions);
+
+      // create variable to hold list item labels
+      var htmlOptionsList = '';
+
+      // for each pair of options, we're pulling the label and inserting it into html string
+      inputArea.options.forEach(function (pair) {
+        htmlOptionsList += "<option value='" + pair.label + "'>";
+        console.log(htmlOptionsList);
+      });
+
+      // get input name id of
+      var div_content = (0, _jquery2['default'])('#' + inputArea.id);
+
+      // adding closing tags to the datalist and div
+      var finalHTMLOutputList = htmlOptionsList + "</datalist></div>";
+
+      // appending whole html string to the dom element we selected earlier
+      div_content.append(finalHTMLOutputList);
+
+      // function getOptions( input ){
+      //   return input.label;
+      // }
+      // console.log(getOptions());
+      // inputArea.forEach( getOptions(){
+      //
+      // }){
+      //   var htmlOptionsList = formOptionList( inputArea.options );
+      //   console.log(htmlOptionsList);
+      //   form_content.append(htmlOptions);
+      // };
+      //
+      // form_content.append(htmlOptionsList + '<datalist>' + htmlOptions + '</datalist>')
+    }
   });
 });
 
--- src/js/main.js
+++ src/js/main.js
@@ -10,10 +10,42 @@ var baseURL = 'http://json-data.herokuapp.com/forms'
 // get form content area in dom
 var form_content = $('.form_content');
 
-// form template
-function formTemplate ( type, label, id, icon, options){
+
+// form textarea
+function formTemplateTextarea ( type, label, id, icon, options){
   return `
-  <p>${type} ${label}
+  <div id="${id}">
+    <span class="fa ${icon}"></span>
+    <input placeholder="${label}" type="${type}" />
+  </div>
+  `
+}
+
+
+// form template
+function formTemplate ( type, label, id, icon){
+  return `
+  <div id="${id}">
+    <span class="fa ${icon}"></span>
+    <input placeholder="${label}" type="${type}" />
+  </div>
+  `
+}
+
+
+// form template for options
+function formTemplateOptions ( type, label, id, icon, options){
+  return `
+  <div class="${id}">
+    <input list="${id}" type="${type}" name="${label}" placeholder="${label}" /></label>
+    <datalist id=${id}>
+  `
+}
+
+// form template for options
+function formOptionList ( option ){
+  return `
+  <option value="${option}">
   `
 }
 
@@ -26,8 +58,56 @@ request.then(
   function (response) {
     //console.log(response);
     response.forEach(function (inputArea){
-      var html = formTemplate(inputArea.type, inputArea.label);
-      form_content.append(html);
+
+      if (inputArea.type === 'textarea') {
+        var htmlTextarea = formTemplateTextarea(inputArea.type, inputArea.label, inputArea.id, inputArea.icon, inputArea.options);
+        form_content.append(htmlTextarea);
+      } else if ( inputArea.options.length === 0 ) {
+        var html = formTemplate(inputArea.type, inputArea.label, inputArea.id, inputArea.icon);
+        form_content.append(html);
+      } else {
+        // stores results of formTemplateOptions function above in htmlOptions variable
+        var htmlOptions = formTemplateOptions(inputArea.type, inputArea.label, inputArea.id, inputArea.icon, inputArea.options);
+
+        // add initial input tag via htmlOptions
+        form_content.append(htmlOptions);
+
+        // create variable to hold list item labels
+        var htmlOptionsList = '';
+
+        // for each pair of options, we're pulling the label and inserting it into html string
+        inputArea.options.forEach(function ( pair ){
+          htmlOptionsList += ("<option value='" + pair.label + "'>");
+          console.log(htmlOptionsList);
+        });
+
+        // get input name id of
+        var div_content = $('#' + inputArea.id );
+
+
+        // adding closing tags to the datalist and div
+        var finalHTMLOutputList = htmlOptionsList + "</datalist></div>"
+
+        // appending whole html string to the dom element we selected earlier
+        div_content.append(finalHTMLOutputList);
+
+
+        // function getOptions( input ){
+        //   return input.label;
+        // }
+        // console.log(getOptions());
+        // inputArea.forEach( getOptions(){
+        //
+        // }){
+        //   var htmlOptionsList = formOptionList( inputArea.options );
+        //   console.log(htmlOptionsList);
+        //   form_content.append(htmlOptions);
+        // };
+        //
+        // form_content.append(htmlOptionsList + '<datalist>' + htmlOptions + '</datalist>')
+
+
+      }
     });
 
 });
